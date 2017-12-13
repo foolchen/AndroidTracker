d63c80abda13eaf9fc59d47ce2daa96238a56ddf
陈冲 <chenchong8806@gmail.com>
2017-12-13 10:59:17 +0800

    增加了对应的注释

--- app/src/main/java/com/foolchen/lib/tracker/demo/App.kt
+++ app/src/main/java/com/foolchen/lib/tracker/demo/App.kt
@@ -15,11 +15,19 @@ import com.foolchen.lib.tracker.lifecycle.ITrackerContext
 class App : Application(), ITrackerContext {
   override fun onCreate() {
     super.onCreate()
+    // 设定一些通用的属性，这些属性在每次统计事件中都会附带
+    // 注意：如果此处的属性名与内置属性的名称相同，则内置属性会被覆盖
     Tracker.addProperty("附加的属性1", "附加的属性1")
     Tracker.addProperty("附加的属性2", "附加的属性2")
-    Tracker.setService(BuildConfig.SERVICE_HOST, BuildConfig.SERVICE_PATH)
-    Tracker.setProjectName(BuildConfig.PROJECT_NAME)
-    Tracker.setMode(TrackerMode.DEBUG_ONLY)
+    // 设定上报数据的主机和接口
+    // 注意：该方法一定要在Tracker.initialize()方法前调用
+    // 否则会由于上报地址未初始化，在触发启动事件时导致崩溃
+    Tracker.setService("https://www.demo.com", "report.php")
+    // 设定上报数据的项目名称
+    Tracker.setProjectName("android_tracker")
+    // 设定上报数据的模式
+    Tracker.setMode(TrackerMode.RELEASE)
+    // 初始化AndroidTracker
     Tracker.initialize(this)
 
     DebugDB.getAddressLog()
